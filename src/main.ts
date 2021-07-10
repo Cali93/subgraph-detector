f5e4f304d17099ac397b62f454f96a0ba821fda6
Cali93 <armut.cumaali@gmail.com>
2021-07-10 20:43:32 +0200

    misc. improvements

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { AppService } from './app.service';
-
+import helmet from 'helmet';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet());
+  app.enableCors();
   const theGraphService = app.select(AppModule).get(AppService, { strict: true });
   theGraphService.pollLatestSubgraphs();
   await app.listen(3000);
