bb4f300598ce493ec83800ed1be68fe817d99ba8
Cali93 <armut.cumaali@gmail.com>
2021-07-10 20:36:25 +0200

    removed app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,19 @@
 import { HttpModule, Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 
-import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import config from './configs/config';
 import { NotifyModule } from './notify/notify.module';
 
 @Module({
-  imports: [HttpModule, NotifyModule, ConfigModule.forRoot({ isGlobal: true, load: [config] }),],
-  controllers: [AppController],
+  imports: [
+    HttpModule.register({
+      timeout: 5000,
+      maxRedirects: 5,
+    }),
+    NotifyModule,
+    ConfigModule.forRoot({ isGlobal: true, load: [config] }),
+  ],
   providers: [AppService, ConfigService],
 })
-export class AppModule {}
+export class AppModule { }
